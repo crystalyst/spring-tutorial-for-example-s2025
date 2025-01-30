9573d0ea79fa2a7a4ed1bc8ce7883d3cd7ccb184
Je Seung You (Jason) <jy2908@columbia.edu>
2025-01-29 21:40:26 -0500

    first commit springboot

---
+++ src/main/java/com/example/frameworkstudycrud/FrameWorkStudyCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.frameworkstudycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameWorkStudyCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameWorkStudyCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/frameworkstudycrud/FrameWorkStudyCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frameworkstudycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameWorkStudyCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
